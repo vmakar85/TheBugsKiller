c5803b52b351d5d226adf1ef1a363b61ec9366bf
Ksenia <ksun19@mail.ru>
2019-10-04 01:14:29 +0300

    z1

---
+++ TBK/android/src/com/blogpost/mybinaryway/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.blogpost.mybinaryway;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.blogpost.mybinaryway.TheBugsKiller;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new TheBugsKiller(), config);
+	}
+}
