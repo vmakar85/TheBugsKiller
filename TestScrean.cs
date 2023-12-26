9cafe005c8361284928c3c9ff8f5d179411bcc35
Vladimir <vmakar85@gmail.com>
2023-12-26 18:08:06 +0300

    add bug animation + btn

--- TestScrean.cs
+++ TestScrean.cs
@@ -6,26 +6,31 @@ public partial class TestScrean : Node2D
 	AudioStreamPlayer shotSound;
 	AudioStreamPlayer smashSound;
 	Button button;
-	RichTextLabel lText; 
+	RichTextLabel lText;
+	AnimatedSprite2D spriteTest;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		shotSound = GetNode<AudioStreamPlayer>(path: "/root/Main/TestScrean/ShotSound");
 		smashSound = GetNode<AudioStreamPlayer>(path: "/root/Main/TestScrean/SmashSound");
-		button = GetNode<Button>(path: "/root/Main/TestScrean/ShotBtn");
 		lText = GetNode<RichTextLabel>(path: "/root/Main/TestScrean/RichTextLabel");
+		spriteTest = GetNode<AnimatedSprite2D>(path: "/root/Main/TestScrean/BugSprite");
+		
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		int r = Godot.GD.RandRange(0,1);
-		int g = Godot.GD.RandRange(0,1);
-		int b = Godot.GD.RandRange(0,1);
-		lText.AddThemeColorOverride("default_color", new Color(r,g,b,1));
+		int r = Godot.GD.RandRange(0, 1);
+		int g = Godot.GD.RandRange(0, 1);
+		int b = Godot.GD.RandRange(0, 1);
+		lText.AddThemeColorOverride("default_color", new Color(r, g, b, 1));
 		//Godot.GD.Print("r: " + r + " g: " + g + " b: " + b);
+		Godot.GD.Print("delta: " + delta);
+
 	}
-	
+
 	private void _on_shot_btn_pressed()
 	{
 		shotSound.Play();
@@ -34,5 +39,25 @@ public partial class TestScrean : Node2D
 	{
 		smashSound.Play();
 	}
+	private void _on_dead_btn_pressed()
+	{
+		spriteTest.Animation = "dead";
+	}
+	private void _on_run_btn_pressed()
+	{
+		spriteTest.Animation = "run";
+		spriteTest.Play();
+	}
+	private void _on_bugs_timer_timeout()
+	{
+		// Replace with function body.
+	}
+
 }
+
+
+
+
+
+
 
