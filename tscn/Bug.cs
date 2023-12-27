135de73b18e3b3277ae2e48b478436cd3ce63d12
Vladimir <vmakar85@gmail.com>
2023-12-28 01:25:19 +0300

    add gameplay stuff

---
+++ tscn/Bug.cs
@@ -0,0 +1,33 @@
+using Godot;
+using System;
+
+public partial class Bug : RigidBody2D
+{
+	AnimatedSprite2D animatedSprite2D;
+	AudioStreamPlayer smashSound;
+	Boolean isDead = false;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	 animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+	 smashSound = GetNode<AudioStreamPlayer>("SmashSound");
+	}
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+	private void _on_visible_on_screen_notifier_2d_screen_exited()
+	{
+		QueueFree();
+	}
+	private void _on_area_2d_input_event(Node viewport, InputEvent @event, long shape_idx)
+	{
+		if (@event is InputEventScreenTouch && !isDead) {
+			isDead = true;	
+			animatedSprite2D.Play("dead");
+			LinearVelocity = Vector2.Zero;
+			smashSound.Play();
+			Godot.GD.Print("bzz bzz bzzz....."); 
+		}
+	}
+}
---
+++ tscn/main.cs
@@ -0,0 +1,47 @@
+using Godot;
+using System;
+
+public partial class main : Node2D
+{
+	PackedScene scene = GD.Load<PackedScene>("res://tscn/Bug.tscn");
+	AnimatedSprite2D animatedSprite2D;
+	Bug mob;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+	private void _on_mob_timer_timeout()
+	{
+		mob = (Bug)scene.Instantiate();
+		// Choose a random location on Path2D.
+		var mobSpawnLocation = GetNode<PathFollow2D>("MobPath/MobSpawnLocation");
+		mobSpawnLocation.ProgressRatio = GD.Randf();
+		// Set the mob's direction perpendicular to the path direction.
+		float direction = mobSpawnLocation.Rotation + Mathf.Pi / 2;
+		// Set the mob's position to a random location.
+		mob.Position = mobSpawnLocation.Position;
+		// Add some randomness to the direction.
+		direction += (float)GD.RandRange(-Mathf.Pi / 4, Mathf.Pi / 4);
+		//var rE = direction - 30;
+		mob.Rotation = direction;
+		// Choose the velocity.
+		var velocity = new Vector2((float)GD.RandRange(150.0, 250.0), 0);
+		mob.LinearVelocity = velocity.Rotated(direction);
+		// Set animation.
+		animatedSprite2D = mob.GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+		animatedSprite2D.Play("run");
+		// Spawn the mob by adding it to the Main scene.
+		AddChild(mob);
+		//Godot.GD.Print("timer: bzz bzz bzzz.....");
+	}
+}
+
+
+
+
+
+
