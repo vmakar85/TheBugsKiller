384612ac291c076643ebdc2fdddf2798ed6f8f2c
Vladimir <vmakar85@gmail.com>
2023-12-29 01:13:54 +0300

    add some particle

--- tscn/Bug.cs
+++ tscn/Bug.cs
@@ -5,13 +5,22 @@ public partial class Bug : RigidBody2D
 {
 	AnimatedSprite2D animatedSprite2D;
 	AudioStreamPlayer smashSound;
+	AudioStreamPlayer missSound;
 	AnimationPlayer animationPlayer;
+	Godot.CpuParticles2D cpuParticles2D;
+	//Godot.GpuParticles2D gpuParticles2D;
+
 	Boolean isDead = false;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-	 animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
-	 smashSound = GetNode<AudioStreamPlayer>("SmashSound");
+		animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+		smashSound = GetNode<AudioStreamPlayer>("SmashSound");
+		missSound = GetNode<AudioStreamPlayer>("MissSound");
+		cpuParticles2D = GetNode<Godot.CpuParticles2D>("AnimatedSprite2D/CPUParticles2D");
+		//gpuParticles2D = GetNode<Godot.GpuParticles2D>("AnimatedSprite2D/GPUParticles2D");
+
 
 	}
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -24,14 +33,25 @@ public partial class Bug : RigidBody2D
 	}
 	private void _on_area_2d_input_event(Node viewport, InputEvent @event, long shape_idx)
 	{
-		if (@event is InputEventScreenTouch && !isDead) {
-			isDead = true;	
+		if (@event is InputEventScreenTouch && !isDead && !missSound.Playing)
+		{
+			cpuParticles2D.Emitting = true;
+			isDead = true;
 			animatedSprite2D.Play("dead");
 			LinearVelocity = Vector2.Zero;
 			smashSound.Play();
 			Input.VibrateHandheld(50);
-			Godot.GD.Print("bzz bzz bzzz....."); 
+			//	
+			
+			Godot.GD.Print("bzz bzz bzzz.....");
 
+		}
+	}
+	private void _on_area_2d_miss_input_event(Node viewport, InputEvent @event, long shape_idx)
+	{
+		if (@event is InputEventScreenTouch && !isDead && !missSound.Playing)
+		{
+			missSound.Play();
 		}
 	}
 }
